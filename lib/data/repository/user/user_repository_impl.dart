34298717c23bb75c58fdbed163baa548bb196e20
wied artha pratama <wiedarthapratama@gmail.com>
2023-01-08 12:32:20 +0700

    ubah password

--- lib/data/repository/user/user_repository_impl.dart
+++ lib/data/repository/user/user_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:skripsi_wap/data/data_source/user/remote_data_source.dart';
 import 'package:skripsi_wap/data/exception/exception.dart';
 import 'package:skripsi_wap/data/model/user/user_model.dart';
 import 'package:dartz/dartz.dart';
+import 'package:skripsi_wap/data/response/base_response.dart';
 import 'package:skripsi_wap/domain/repository/user/user_repository.dart';
 
 class UserRepositoryImpl implements UserRepository {
@@ -15,6 +16,42 @@ class UserRepositoryImpl implements UserRepository {
   Future<Either<WException, UserModel>> getProfile() async {
     try {
       final response = await remoteDataSource.getProfile();
+
+      return Right(response);
+    } on DioError catch (e) {
+      return Left(e.exception);
+    } catch (_) {
+      return const Left(WException.internalServerException());
+    }
+  }
+
+  @override
+  Future<Either<WException, BaseResponse>> saveProfile(
+      {required String nama,
+      required String email,
+      required String telepon}) async {
+    try {
+      final response = await remoteDataSource.saveProfile(
+          nama: nama, email: email, telepon: telepon);
+
+      return Right(response);
+    } on DioError catch (e) {
+      return Left(e.exception);
+    } catch (_) {
+      return const Left(WException.internalServerException());
+    }
+  }
+
+  @override
+  Future<Either<WException, BaseResponse>> savePassword(
+      {required String passwordOld,
+      required String passwordNew,
+      required String passwordConfirmation}) async {
+    try {
+      final response = await remoteDataSource.savePassword(
+          passwordOld: passwordOld,
+          passwordNew: passwordNew,
+          passwordConfirmation: passwordConfirmation);
 
       return Right(response);
     } on DioError catch (e) {
--- lib/presentation/screen/ubah_profile/ubah_profile_screen.dart
+++ lib/presentation/screen/ubah_profile/ubah_profile_screen.dart
@@ -4,13 +4,16 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_exif_rotation/flutter_exif_rotation.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 import 'package:skripsi_wap/common/extension/extension.dart';
 import 'package:skripsi_wap/common/style/style.dart';
+import 'package:skripsi_wap/presentation/viewmodel/user/user_viewmodel.dart';
 import 'package:skripsi_wap/presentation/widget/appbar/appbar.dart';
 import 'package:skripsi_wap/presentation/widget/button/button.dart';
 import 'package:skripsi_wap/presentation/widget/spacing/spacing.dart';
 import 'package:skripsi_wap/presentation/widget/textfield/textfield.dart';
 import 'package:skripsi_wap/service/navigation_service.dart';
+import 'package:skripsi_wap/util/validator/validator.dart';
 
 class UbahProfileScreen extends StatefulWidget {
   const UbahProfileScreen({Key? key}) : super(key: key);
@@ -29,47 +32,85 @@ class _UbahProfileScreenState extends State<UbahProfileScreen> {
         title: 'Aplikasi',
         elevation: .2,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'UbahProfile Kos',
-              style: WTextStyle.headline3.bold,
-            ),
-            WSpacing.vertical.size16,
-            const WPrimaryTextField(label: 'Nama'),
-            WSpacing.vertical.size16,
-            const WPrimaryTextField(label: 'Email'),
-            WSpacing.vertical.size16,
-            const WPrimaryTextField(label: 'Telepon'),
-            WSpacing.vertical.size8,
-            WPrimaryTextField(
-              label: 'Foto Pribadi',
-              enable: false,
-              onTap: () async {
-                var image = await ImagePicker()
-                    .pickImage(source: ImageSource.camera, imageQuality: 75);
+      body: ChangeNotifierProvider<UserViewModel>(
+        create: (_) => UserViewModel()..init(),
+        child: Consumer<UserViewModel>(builder: (_, viewModel, __) {
+          if (viewModel.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Form(
+            key: viewModel.formKey,
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'UbahProfile Kos',
+                    style: WTextStyle.headline3.bold,
+                  ),
+                  WSpacing.vertical.size16,
+                  WPrimaryTextField(
+                    label: 'Nama',
+                    controller: viewModel.namaController,
+                    validator: (value) => (value ?? '').isEmpty
+                        ? 'Judul masih kosong'
+                        : value!.hasEmoji
+                            ? 'Tidak boleh mengandung emoji'
+                            : null,
+                  ),
+                  WSpacing.vertical.size16,
+                  WPrimaryTextField(
+                    label: 'Email',
+                    controller: viewModel.emailController,
+                    validator: (value) => (value ?? '').isEmpty
+                        ? 'Judul masih kosong'
+                        : value!.hasEmoji
+                            ? 'Tidak boleh mengandung emoji'
+                            : null,
+                  ),
+                  WSpacing.vertical.size16,
+                  WPrimaryTextField(
+                    label: 'Telepon',
+                    controller: viewModel.teleponController,
+                    validator: (value) => (value ?? '').isEmpty
+                        ? 'Judul masih kosong'
+                        : value!.hasEmoji
+                            ? 'Tidak boleh mengandung emoji'
+                            : null,
+                  ),
+                  WSpacing.vertical.size8,
+                  // WPrimaryTextField(
+                  //   label: 'Foto Pribadi',
+                  //   enable: false,
+                  //   onTap: () async {
+                  //     var image = await ImagePicker()
+                  //         .pickImage(source: ImageSource.camera, imageQuality: 75);
 
-                if (image != null) {
-                  File? file =
-                      await FlutterExifRotation.rotateImage(path: image.path);
+                  //     if (image != null) {
+                  //       File? file =
+                  //           await FlutterExifRotation.rotateImage(path: image.path);
 
-                  log('berhasil: $file');
-                }
-              },
-            ),
-            WSpacing.vertical.size8,
-            Center(
-              child: WPrimaryButton(
-                title: 'Kirim',
-                onTap: () => NavigationService().router.navigateBack(),
-                fullWidth: false,
+                  //       log('berhasil: $file');
+                  //     }
+                  //   },
+                  // ),
+                  // WSpacing.vertical.size8,
+                  Center(
+                    child: WPrimaryButton(
+                      title: 'Kirim',
+                      onTap: viewModel.onSave,
+                      fullWidth: false,
+                      isLoading: viewModel.isLoadingCTA,
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          );
+        }),
       ),
     );
   }
